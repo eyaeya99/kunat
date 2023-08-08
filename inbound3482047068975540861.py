b9dcd77c725825bd5b27876c6a3e1e96d96248c5
eyaeya99 <101121231+eyaeya99@users.noreply.github.com>
2023-08-08 21:06:10 +0800

    Add files via upload

---
+++ inbound3482047068975540861.py
@@ -0,0 +1,7 @@
+print ("Welcome to programming")
+print ("Welcome to parallel programming and Machine Learning")
+print ("Welcome to BS Computer Science 4")
+print ("Welcome to BS Computer Science 4")
+print ("CCSICT Echague Campus")
+
+
