fae1ea2b00a2530dc41bc0e3438dcbcbb8f0f55a
eyaeya99 <101121231+eyaeya99@users.noreply.github.com>
2023-08-08 21:13:54 +0800

    Add files via upload

---
+++ inbound5671887273593466086.py
@@ -0,0 +1,12 @@
+age = 48
+txt = "My name is Freya, and I am {}"
+print(txt.format(age))
+
+quantity = 5
+unitcode = 102
+price = 546.55
+myorder = "I will buy {} pieces of product {} for {} pesos."
+print(myorder.format(quantity, unitcode, price))
+
+
+
