9bd2816898bf81f6d6a4584a5583d3aafddc2210
eyaeya99 <101121231+eyaeya99@users.noreply.github.com>
2023-08-08 21:16:38 +0800

    Add files via upload

---
+++ inbound959213057220228098.py
@@ -0,0 +1,19 @@
+a= 10
+b= 100
+c= 100
+if a > b and a > c:
+    print(str(a) + " is greater than " + str(b) + "and" + str(c))
+elif b > a and b > c:
+    print(str(b) + " is greater than " + str(a) + " and " + str(c))
+elif c > a and c > b:
+    print(str(c) + " is greater than " + str(a) + " and " + str(b))
+elif a == b and a > c:
+    print(str(a) + " is equal to " + str(b) + " and greater than " + str(c))
+elif a == c and a > b:
+    print(str(a) + " is equal to " + str(c) + " and greater than " + str(b))
+elif b == c and b > a:
+    print(str(b) + " is equal to " + str(c) + " and greater than " + str(a))
+else:
+    print("they are all equal")
+    
+    
