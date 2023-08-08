4322670e3be127e58e4ce64c7cf0d0e7b35bced8
eyaeya99 <101121231+eyaeya99@users.noreply.github.com>
2023-08-08 21:11:32 +0800

    Add files via upload

---
+++ inbound4684948552585623181.py
@@ -0,0 +1,48 @@
+x="Parallel Programming in Python"
+y=5
+z=3+y
+print (x)
+print (y)
+print (z)
+
+x=str (100)
+y=int (50)
+z=float (20)
+
+print (x)
+print (y)
+print (z)
+
+x= 200
+y="Parallel Programming"
+print (type (x))
+print (type (y))
+print (type (z))
+
+x,y,z = "Image Processing", "Machine Learning", "Deep Learning"
+a,b,c = 12.55, 'CCSICT', "200+5"
+
+print (x)
+print (y)
+print (z)
+print (a)
+print (b)
+print (c)
+
+x= 128
+y= 2.54
+z= 10j
+
+a= float (x)
+
+b= int (y)
+
+c= complex (x)
+
+print (a)
+print (b)
+print (c)
+
+print (type (a))
+print (type (b))
+print (type (c))
