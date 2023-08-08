af06870d6a4e3767300c9d6e791c27d139418356
eyaeya99 <101121231+eyaeya99@users.noreply.github.com>
2023-08-08 21:15:55 +0800

    Add files via upload

---
+++ inbound8021304202525327718.py
@@ -0,0 +1,20 @@
+a=10
+b=6
+if a > b:    
+    print("a is greater than b")
+
+x = 20
+y = 50
+if y > x:
+    print(str(x) + " is greater than " + str(y))
+else:
+    print(str(x) + " is not greater than " + str(x))
+una = 100
+dos = 100
+if una > dos:
+    print(str(una) + " > " + str(dos))
+    
+elif dos == una:
+    print(str(una) + " == " + str(dos))
+else:
+    print(str(una) + " < " + str(dos))
